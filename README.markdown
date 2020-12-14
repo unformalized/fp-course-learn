14de37f40fd2ac87a092536c9b6b424fc000401e
liubin <1419831251@qq.com>
2020-12-14 23:31:05 +0800

    feat: fp-course 完成第一节

---
+++ README.markdown
@@ -0,0 +1,2 @@
+源项目地址：https://github.com/tonymorris/fp-course
+本项目仅用个人学习
---
+++ src/Course/Optional.hs
@@ -0,0 +1,130 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Course.Optional where
+
+import qualified Control.Applicative as A
+import qualified Control.Monad as M
+import Course.Core
+import qualified Prelude as P
+
+-- | The `Optional` data type contains 0 or 1 value.
+--
+-- It might be thought of as a list, with a maximum length of one.
+data Optional a =
+  Full a
+  | Empty
+  deriving (Eq, Show)
+
+-- | Map the given function on the possible value.
+--
+-- >>> mapOptional (+1) Empty
+-- Empty
+--
+-- >>> mapOptional (+1) (Full 8)
+-- Full 9
+-- Functor
+mapOptional ::
+  (a -> b)
+  -> Optional a
+  -> Optional b
+mapOptional f (Full a) = Full (f a)
+mapOptional f Empty =  Empty
+
+-- | Bind the given function on the possible value.
+--
+-- >>> bindOptional Full Empty
+-- Empty
+--
+-- >>> bindOptional (\n -> if even n then Full (n - 1) else Full (n + 1)) (Full 8)
+-- Full 7
+--
+-- >>> bindOptional (\n -> if even n then Full (n - 1) else Full (n + 1)) (Full 9)
+-- Full 10
+-- Monad
+bindOptional ::
+  (a -> Optional b)
+  -> Optional a
+  -> Optional b
+bindOptional f (Full a) = f a
+bindOptional f Empty = Empty
+
+-- | Return the possible value if it exists; otherwise, the second argument.
+--
+-- >>> Full 8 ?? 99
+-- 8
+--
+-- >>> Empty ?? 99
+-- 99
+(??) ::
+  Optional a
+  -> a
+  -> a
+(??) (Full a) _ = a
+(??) _ a = a
+
+-- | Try the first optional for a value. If it has a value, use it; otherwise,
+-- use the second value.
+--
+-- >>> Full 8 <+> Empty
+-- Full 8
+--
+-- >>> Full 8 <+> Full 9
+-- Full 8
+--
+-- >>> Empty <+> Full 9
+-- Full 9
+--
+-- >>> Empty <+> Empty
+-- Empty
+(<+>) ::
+  Optional a
+  -> Optional a
+  -> Optional a
+(<+>) (Full a) _ = Full a
+(<+>) _ b = b
+
+-- | Replaces the Full and Empty constructors in an optional.
+--
+-- >>> optional (+1) 0 (Full 8)
+-- 9
+--
+-- >>> optional (+1) 0 Empty
+-- 0
+optional ::
+  (a -> b)
+  -> b
+  -> Optional a
+  -> b
+optional f b (Full x) = f x
+optional f b _  = b
+
+-- Applicative
+-- bind :: (a -> O b) -> O a -> O b
+-- map :: (a -> b) -> O a -> O b
+-- apply :: O (a -> b) -> O a -> O b
+applyOptional :: Optional (a -> b) -> Optional a -> Optional b
+applyOptional f a = bindOptional (\f' -> mapOptional f' a) f
+
+twiceOptional :: (a -> b -> c) -> Optional a -> Optional b -> Optional c
+twiceOptional f = applyOptional . mapOptional f
+
+contains :: Eq a => a -> Optional a -> Bool
+contains _ Empty = False
+contains a (Full z) = a == z
+
+instance P.Functor Optional where
+  fmap =
+    M.liftM
+
+instance A.Applicative Optional where
+  (<*>) =
+    M.ap
+  pure =
+    Full
+
+instance P.Monad Optional where
+  (>>=) =
+    flip bindOptional
+  return =
+    Full
