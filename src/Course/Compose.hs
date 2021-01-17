20b4765015272130540b2d7255fd77714702c650
BestMasterLiubin <bestmasterliubin@gmail.com>
2021-01-17 22:37:22 +0800

    feat: Compose 完成

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module Course.Compose where
 
@@ -17,23 +18,40 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  (<$>) ::
+    (a -> b)
+    -> Compose f g a
+    -> Compose f g b
+  (<$>) f (Compose fga) =
+    Compose ((f <$>) <$> fga)
+
+
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
   pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+    Compose . pure . pure
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  (<*>) ::
+    Compose f g (a -> b)
+    -> Compose f g a
+    -> Compose f g b
+  (<*>) (Compose fgab) (Compose fga) =
+    Compose (lift2 (<*>) fgab fga)
 
-instance (Monad f, Monad g) =>
-  Monad (Compose f g) where
--- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (=<<)#instance (Compose f g)"
+-- Compose can't instance Monad
+--instance (Monad f, Monad g) =>
+--  Monad (Compose f g) where
+---- Implement the (=<<) function for a Monad instance for Compose
+--  (=<<) ::
+--    (a -> Compose f g b)
+--    -> Compose f g a
+--    -> Compose f g b
+--  (=<<) f (Compose fga) =
+--    Compose (fga >>= \ga -> _todo)
+--
+-- _todo :: f (g b)
 
 -- Note that the inner g is Contravariant but the outer f is
 -- Functor. We would not be able to write an instance if both were
